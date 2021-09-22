6ff8ab318c80528ef7f84c222aa5cde215453690
Jacob Gutierrez <jacob.gutierrez@newmill.com>
2021-09-22 16:40:14 -0500

    codigo base para componente de busqueda por paises

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class PorPaisComponent implements OnInit {
+export class PorPaisComponent {
+  public termino: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  public buscar() {
+    console.log(this.termino);
   }
-
 }
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styles: [
+    `
+      li {
+        cursor: pointer;
+      }
+
+      li:hover {
+        background-color: #63acd3;
+      }
+    `,
+  ],
 })
 export class SidebarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
